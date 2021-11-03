6acf5218f898279b6890e003a6a5a799ed5a57ab
Jarod SENGKEO <j.sengkeo@student.isartdigital.com>
2021-11-03 09:47:35 +0100

    first commit

---
+++ Source/gladiator/PlayerPawn.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayerPawn.h"
+
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	capsule = CreateDefaultSubobject<UCapsuleComponent>("capsule collider");
+	RootComponent = capsule;
+
+	skmesh = CreateDefaultSubobject<USkeletalMeshComponent>("skeletal mesh");
+	skmesh->SetupAttachment(RootComponent);
+
+	cameraBoom = CreateDefaultSubobject<USpringArmComponent>("spring arm");
+	cameraBoom->SetupAttachment(RootComponent);
+
+	camera = CreateDefaultSubobject<UCameraComponent>("camera");
+	camera->SetupAttachment(cameraBoom, USpringArmComponent::SocketName);
+	camera->SetActive(true);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	characterMovement = CreateDefaultSubobject<UCharacterMovementComponent>("character movement");
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/gladiator/PlayerPawn.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class GLADIATOR_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleAnywhere)
+		class UCapsuleComponent* capsule;
+
+	UPROPERTY(EditAnywhere)
+		USkeletalMeshComponent* skmesh;
+	UPROPERTY(EditAnywhere)
+		float speed = 1.f;
+
+	UPROPERTY(VisibleAnywhere)
+		class USpringArmComponent*			cameraBoom;
+	UPROPERTY(VisibleAnywhere)
+		class UCameraComponent*				camera;
+	UPROPERTY(VisibleAnywhere)
+		class UCharacterMovementComponent*	characterMovement;
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};

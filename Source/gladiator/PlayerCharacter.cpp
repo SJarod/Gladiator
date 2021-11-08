3d62b8e638f097e1b79a3399992cc5ec6a9f329b
Loan CAMPAN <l.campan@student.isartdigital.com>
2021-11-08 14:20:50 +0100

    created hammer overlap function

--- Source/gladiator/PlayerCharacter.cpp
+++ Source/gladiator/PlayerCharacter.cpp
@@ -188,6 +188,7 @@ APlayerCharacter::APlayerCharacter()
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	hammerCollider->OnComponentBeginOverlap.AddDynamic(this, &APlayerCharacter::OnHammerBeginOverlap);
 }
 
 // Called to bind functionality to input
@@ -210,4 +211,10 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	PlayerInputComponent->BindAction("Attack", IE_Pressed, this, &APlayerCharacter::attack);
 	PlayerInputComponent->BindAction("Block", IE_Pressed, this, &APlayerCharacter::block);
 	PlayerInputComponent->BindAction("Block", IE_Released, this, &APlayerCharacter::unblock);
+}
+
+void APlayerCharacter::OnHammerBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(attacking)
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("HammerHit"));
 }
--- Source/gladiator/PlayerCharacter.h
+++ Source/gladiator/PlayerCharacter.h
@@ -75,6 +75,9 @@ public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
 
+	UFUNCTION()
+		void OnHammerBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
